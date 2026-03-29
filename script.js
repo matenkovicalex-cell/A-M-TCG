b5c909c1bbebbaffd17b4dc5203b9dbf93c322d6
matenkovicalex-cell <matenkovicalex@gmail.com>
2026-03-29 17:56:14 +0200

    Refactor toggleMode and add new functions

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* Tmavý / svetlý mód */
 function toggleMode() {
   document.body.classList.toggle("dark");
+}
+
+/* Scroll na galériu */
+function scrollToGallery() {
+  const gallery = document.querySelector('.grid');
+  gallery.scrollIntoView({ behavior: 'smooth' });
+}
+
+/* TikTok presmerovanie */
+function goToTikTok() {
+  window.open("https://www.tiktok.com/@ampokemon", "_blank");
 }
